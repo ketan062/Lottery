349eda4c89f94964f7925c366c2e24158fc3828c
ketan062 <156174548+ketan062@users.noreply.github.com>
2024-05-16 18:54:31 +0530

    lottery.js

--- lottery.js
+++ lottery.js
@@ -1,13 +1,10 @@
-let tic1 = document.getElementById("ticket1")
-let tic2 = document.getElementById("ticket2")
-let tic3 = document.getElementById("ticket3")
-let tic4 = document.getElementById("ticket4")
-let tic5 = document.getElementById("ticket5")
+let lottery = document.querySelectorAll('.lotteryno')
 let result = document.querySelector(".headline")
 let empty = document.querySelector(".input")
 let retry = document.getElementById("button")
 let amount = document.getElementById("amt")
 let x=10;
+amount.innerHTML = x;
 
 retry.addEventListener("click", () =>{
     empty.style.opacity="100";
@@ -15,80 +12,30 @@ retry.addEventListener("click", () =>{
     retry.style.opacity="0";
     newgame()
 })
+
 newgame()
-
 function newgame() {
-    tic1.innerHTML = Math.floor(Math.random() * 100 + 1)
-    tic2.innerHTML = Math.floor(Math.random() * 100 + 1)
-    tic3.innerHTML = Math.floor(Math.random() * 100 + 1)
-    tic4.innerHTML = Math.floor(Math.random() * 100 + 1)
-    tic5.innerHTML = Math.floor(Math.random() * 100 + 1)
-
-    let arr = [tic1,tic2,tic3,tic4,tic5]
-
-    let num = arr[Math.floor(Math.random() * 5)]
-
-    console.log(num);
-    Math.floor(x--);
-    tic1.addEventListener("click", function(){
-        if(tic1 == num){
-            result.innerHTML = "You Won 5 coins"
-            empty.style.opacity="0";
-            Math.floor(x+=5);
-            retry.style.opacity="100";
-        } else{
-            result.innerHTML = "You lose the game"
-            empty.style.opacity="0";
-            retry.style.opacity="100";
-        }
+    lottery.forEach( function (ticket) {
+        ticket.innerHTML = Math.floor(Math.random() * 100 + 1)
     })
-    tic2.addEventListener("click", function(){
-        if(tic2 == num){
-            result.innerHTML = "You Won 5 coins"
-            empty.style.opacity="0";
-            Math.floor(x+=5);
-            retry.style.opacity="100";
-        } else{
-            result.innerHTML = "You lose the game"
-            empty.style.opacity="0";
-            retry.style.opacity="100";
-        }
+    let num = lottery[Math.floor(Math.random() * 5)]
+    lottery.forEach( function (ticket) {
+        ticket.addEventListener('click',function (e) {
+            e.preventDefault
+            if (ticket == num){
+                result.innerHTML = "You Won 5 coins"
+                empty.style.opacity="0";
+                Math.floor(x+=5);
+                retry.style.opacity="100";
+                amount.innerHTML = x;
+            } 
+            if (ticket != num) {
+                result.innerHTML = "You lose the game"
+                empty.style.opacity="0";
+                retry.style.opacity="100";
+                Math.floor(x--);
+                amount.innerHTML = x;
+            }
+        })
     })
-    tic3.addEventListener("click", function(){
-        if(tic3 == num){
-            result.innerHTML = "You Won 5 coins"
-            empty.style.opacity="0";
-            Math.floor(x+=5);
-            retry.style.opacity="100";
-        } else{
-            result.innerHTML = "You lose the game"
-            empty.style.opacity="0";
-            retry.style.opacity="100";
-        }
-    })
-    tic4.addEventListener("click", function(){
-        if(tic4 == num){
-            result.innerHTML = "You Won 5 coins"
-            empty.style.opacity="0";
-            Math.floor(x+=5);
-            retry.style.opacity="100";
-        } else{
-            result.innerHTML = "You lose the game"
-            empty.style.opacity="0";
-            retry.style.opacity="100";
-        }
-    })
-    tic5.addEventListener("click", function(){
-        if(tic5 == num){
-            result.innerHTML = "You Won 5 coins"
-            empty.style.opacity="0";
-            Math.floor(x+=5);
-            retry.style.opacity="100";
-        } else{
-            result.innerHTML = "You lose the game"
-            empty.style.opacity="0";
-            retry.style.opacity="100";
-        }
-    })
-    amount.innerHTML=x;
 }
